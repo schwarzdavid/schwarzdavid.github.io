a38bd299564591114e1b8f10be52a97e289fb151
David Schwarz <schwarz.david10@gmail.com>
2017-10-16 01:05:56 +0200

    Navigator updated

--- src/js/navigator.js
+++ src/js/navigator.js
@@ -5,7 +5,7 @@
 	// CONSTANTS
 	//********************************************
 	const ALLOWED_PAGES = {
-		NONE: '',
+		HOME: 'home',
 		TEASER: 'teaser',
 		RESUME: 'resume'
 	};
@@ -13,10 +13,8 @@
 	//********************************************
 	// SELECTORS
 	//********************************************
-	const _menuSwitches = Array.from(document.querySelectorAll('[data-menu-switch]'));
-	const _pageAnchors = Array.from(document.querySelectorAll('[data-target]'));
-	const _projectSwitches = Array.from(document.querySelectorAll('[data-project]'));
-	const _resumeSwitches = Array.from(document.querySelectorAll('[data-resume-switch]'));
+	const _links = Array.from(document.querySelectorAll('[data-link]'));
+	const _switches = Array.from(document.querySelectorAll('[data-switch]'));
 	const _master = document.body;
 
 	//********************************************
@@ -30,50 +28,56 @@
 	// FUNCTIONS
 	//********************************************
 	function init() {
-		_menuSwitches.forEach((menuSwitch) => {
-			menuSwitch.addEventListener('click', (e) => {
+		_switches.forEach((_switch) => {
+			_switch.addEventListener('click', (e) => {
 				e.preventDefault();
 
-				if (_master.dataset.active === ALLOWED_PAGES.TEASER) {
-					return closeMenu();
+				const targetOptions = _switch.dataset.switch.split('-');
+				const currOpen = _master.dataset.active;
+				let target = targetOptions[0];
+
+				if(currOpen === targetOptions[0].toString().toLowerCase()){
+					target = targetOptions[1];
 				}
-				return openMenu();
+
+				const page = getPage(target);
+				if(!page){
+					throw new Error(`Illegal page: ${target}`);
+				}
+
+				return openPage(page);
 			});
 		});
 
-		_resumeSwitches.forEach((resumeSwitch) => {
-			resumeSwitch.addEventListener('click', (e) => {
+		_links.forEach((_link) => {
+			_link.addEventListener('click', (e) => {
 				e.preventDefault();
 
-				if(_master.dataset.active === ALLOWED_PAGES.RESUME){
-					return closeResume();
+				const target = _link.dataset.link;
+				const page = getPage(target);
+				if(!page){
+					throw new Error(`Illegal page: ${target}`);
 				}
-				return openResume();
+
+				return openPage(page);
 			});
 		});
+	}
 
-		_pageAnchors.forEach((pageAnchor) => {
+	function getPage(target) {
+		const lowerTarget = target.toString().toLowerCase();
+		let result = '';
+		for(let p in ALLOWED_PAGES){
+			if(ALLOWED_PAGES[p] === lowerTarget){
+				result = p;
+			}
+		}
+		return result;
+	}
 
+	function openPage(page) {
+		requestAnimationFrame(() => {
+			_master.dataset.active = ALLOWED_PAGES[page];
 		});
-
-		_projectSwitches.forEach((projectSwitch) => {
-
-		});
-	}
-
-	function closeMenu() {
-		_master.dataset.active = ALLOWED_PAGES.NONE;
-	}
-
-	function openMenu() {
-		_master.dataset.active = ALLOWED_PAGES.TEASER;
-	}
-
-	function closeResume() {
-		_master.dataset.active = ALLOWED_PAGES.NONE;
-	}
-
-	function openResume() {
-		_master.dataset.active = ALLOWED_PAGES.RESUME;
 	}
 }());
